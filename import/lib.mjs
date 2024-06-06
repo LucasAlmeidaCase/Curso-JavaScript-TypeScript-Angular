0f054f71f97f489bcbb8b0ebfc44e055e159313b
Lucas Almeida <lucas@uaicontrole.com.br>
2024-06-06 15:33:29 -0300

    Dynamic Import parte 2

--- import/lib.mjs
+++ import/lib.mjs
@@ -2,9 +2,3 @@ export const saudacao = () => {
   let data = new Date().toLocaleDateString();
   return `Olá, seja muito bem-vindo. Hoje é ${data}`;
 };
-
-export function getEnderecoByCEP(cep) {
-  return fetch(`https://viacep.com.br/ws/${cep}/json/`).then((response) =>
-    response.json()
-  );
-}
