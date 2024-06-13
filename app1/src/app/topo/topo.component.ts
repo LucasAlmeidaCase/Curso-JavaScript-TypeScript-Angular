44cee767ee517ff2a38abcc1bac5663b641c7258
Lucas Almeida <lucas@uaicontrole.com.br>
2024-06-13 13:56:38 -0300

    Component Templates

--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-topo',
     templateUrl: './topo.component.html'
+    // template: `<p>Esse é o componente Topo</p>`
 })
 export class TopoComponent {}
