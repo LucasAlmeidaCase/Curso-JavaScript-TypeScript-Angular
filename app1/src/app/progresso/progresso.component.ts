3f603c7ccdeb8f131c82d2d99583276c73dec210
Lucas Almeida <lucas@uaicontrole.com.br>
2024-06-13 15:15:27 -0300

    Criando o componente tentativas e progresso e CLI

---
+++ app1/src/app/progresso/progresso.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progresso',
+  templateUrl: './progresso.component.html',
+  styleUrl: './progresso.component.css'
+})
+export class ProgressoComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {
+
+  }
+}
---
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tentativas',
+  templateUrl: './tentativas.component.html',
+  styleUrl: './tentativas.component.css'
+})
+export class TentativasComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {
+
+  }
+}
