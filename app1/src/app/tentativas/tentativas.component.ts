84917c1513ae9e79feb5cb0c6ffe65f8dd358d30
Lucas Almeida <lucas@uaicontrole.com.br>
2024-06-17 10:23:48 -0300

    @Input (property binding entre o componente painel e tentativas)

--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Coracao } from 'app/shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
-  styleUrl: './tentativas.component.css'
+  styleUrl: './tentativas.component.css',
 })
 export class TentativasComponent implements OnInit {
-
-  public coracaoVazio: string = '/assets/coracao_vazio.png'
-  public coracaoCheio: string = '/assets/coracao_cheio.png'
+  @Input() public tentativas: number;
 
   public coracoes: Coracao[] = [
-    new Coracao(true), new Coracao(true), new Coracao(true)
-  ]
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true),
+  ];
 
   constructor() {
     console.log(this.coracoes);
   }
 
   ngOnInit() {
-
+    console.log('tentativas recebidas do painel: ', this.tentativas);
   }
 }
