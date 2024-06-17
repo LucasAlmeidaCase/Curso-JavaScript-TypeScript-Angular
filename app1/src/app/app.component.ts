7de2fd38c35baad6f24adba54ce445ebbaaa8850
Lucas Almeida <lucas@uaicontrole.com.br>
2024-06-17 11:46:30 -0300

    Executando método do componente pai através do componente filho

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app1';
+
+  public encerrarJogo(tipo: string): void {
+    console.log(tipo);
+  }
 }
