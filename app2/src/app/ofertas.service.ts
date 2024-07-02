52bc939c7f76ae3848e5d8c013d1a2531a69af48
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-02 18:01:42 -0300

    Promises - Reject e then

--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -59,7 +59,12 @@ export class OfertasService {
   public getOfertas2(): Promise<Oferta[]> {
     return new Promise((resolve, reject) => {
       // algum tipo de processamento, que ao finalizar, chama a função resolve ou a função reject
-      resolve(this.ofertas);
+      let deu_certo = false;
+      if (deu_certo) {
+        resolve(this.ofertas)
+      } else {
+        reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado'})
+      }
     });
   }
 }
