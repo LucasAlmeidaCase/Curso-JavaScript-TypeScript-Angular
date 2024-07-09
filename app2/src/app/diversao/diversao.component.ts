9e66f4c804cbaae279efb8fe6051b6faa3e0d3d8
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-09 08:08:33 -0300

    Criando as páginas Restaurantes e Diversão

---
+++ app2/src/app/diversao/diversao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diversao',
+  templateUrl: './diversao.component.html',
+  styleUrl: './diversao.component.css'
+})
+export class DiversaoComponent {
+
+}
