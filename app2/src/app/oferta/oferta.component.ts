4a7dc1256e4363509d67a72559760af38a005648
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-09 19:03:04 -0300

    Criando e navegando para a página Oferta

---
+++ app2/src/app/oferta/oferta.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrl: './oferta.component.css',
+})
+export class OfertaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
