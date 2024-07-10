9e3ca62050e55c0e496fb0b362723f656f8cfebb
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-09 21:06:21 -0300

    Criando os componentes ComoUsarComponent e OndeFicaComponent
    ng g c oferta/comoUsar
    ng g c oferta/ondeFica

---
+++ app2/src/app/oferta/como-usar/como-usar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-como-usar',
+  templateUrl: './como-usar.component.html',
+  styleUrl: './como-usar.component.css',
+})
+export class ComoUsarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ app2/src/app/oferta/onde-fica/onde-fica.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onde-fica',
+  templateUrl: './onde-fica.component.html',
+  styleUrl: './onde-fica.component.css',
+})
+export class OndeFicaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
