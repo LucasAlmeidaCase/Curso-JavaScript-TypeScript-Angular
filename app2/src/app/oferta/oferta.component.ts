e8831cbd51efd994fc99a0166f7953f1be5831a4
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-25 20:01:11 -0300

    Observables na prática parte 3 - Um pouco mais a fundo

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable, Observer, interval } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -28,10 +29,27 @@ export class OfertaComponent implements OnInit {
     //   console.log(parametro.id);
     // });
 
-    this.route.params.subscribe({
-      next: (v) => console.log(v),
-      error: (e) => console.log(e),
-      complete: () => console.log('Processamento foi classificado como concluído'),
+    // this.route.params.subscribe({
+    //   next: (v) => console.log(v),
+    //   error: (e) => console.log(e),
+    //   complete: () => console.log('Processamento foi classificado como concluído'),
+    // });
+
+    // let tempo = interval(2000);
+
+    // tempo.subscribe((intervalo: number) => {
+    //   console.log(intervalo);
+    // });
+
+    // observable (observável)
+    let meuObservableTeste = new Observable((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(3);
     });
+
+    // observable (observador)
+    meuObservableTeste.subscribe((resultado: number) =>
+      console.log(resultado + 10)
+    );
   }
 }
