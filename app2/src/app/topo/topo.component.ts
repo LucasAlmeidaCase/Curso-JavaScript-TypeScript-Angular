94d2ffe41d5b78d6cd6430594928741d6f630b7d
Lucas Almeida <lucas@uaicontrole.com.br>
2024-07-26 08:46:55 -0300

    Capturando entrada do usuário - Approach $event e <HTMLInputElement>

--- app2/src/app/topo/topo.component.ts
+++ app2/src/app/topo/topo.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrl: './topo.component.css'
 })
-export class TopoComponent {
+export class TopoComponent implements OnInit{
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  public pesquisa(event: Event): void {
+    console.log((<HTMLInputElement>event.target).value)
+  }
 }
