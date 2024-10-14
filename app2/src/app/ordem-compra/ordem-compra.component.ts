ad9bc7021cb117bbbaeab7862369c15395f69f06
Lucas Almeida <lucas@uaicontrole.com.br>
2024-10-14 08:51:51 -0300

    Validando dados do formulário parte 1 - Aplicando feedback visual nos campos

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -10,27 +10,57 @@ export class OrdemCompraComponent implements OnInit {
   public numero: string = '';
   public complemento: string = '';
   public formaPagamento: string = '';
+
+  //controles de validação dos campos
+  public enderecoValido!: boolean;
+  public numeroValido!: boolean;
+  public complementoValido!: boolean;
+  public formaPagamentoValido!: boolean;
+
   constructor() {}
 
   ngOnInit() {}
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
-    console.log(this.endereco);
+    //console.log(this.endereco)
+
+    //se a string for maior que 3
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizarNumero(numero: string): void {
     this.numero = numero;
-    console.log(this.numero);
+    // console.log(this.numero)
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizarComplemento(complemento: string): void {
     this.complemento = complemento;
-    console.log(this.complemento);
+    // console.log(this.complemento)
+
+    if (this.complemento.length > 0) {
+      this.complementoValido = true;
+    }
   }
 
   public atualizarFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
-    console.log(formaPagamento);
+    // console.log(formaPagamento)
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 }
