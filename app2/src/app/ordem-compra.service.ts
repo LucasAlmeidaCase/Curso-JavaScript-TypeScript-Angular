9380acee65262c358b3656ca8304aaf8335a18fa
Lucas Almeida <lucas@uaicontrole.com.br>
2024-10-14 11:00:02 -0300

    Enviando o formulário via requisição HTTP (POST) para API REST

--- app2/src/app/ordem-compra.service.ts
+++ app2/src/app/ordem-compra.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Pedido } from './shared/pedido.model';
+import { URL_API } from './app.api';
+import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class OrdemCompraService {
   constructor(private http: HttpClient) {}
 
-  public efetivarCompra(pedido: Pedido): void {
-    console.log(pedido);
+  public efetivarCompra(pedido: Pedido): Observable<any> {
+    return this.http
+      .post(`${URL_API}/pedidos`, pedido)
+      .pipe(map((resposta: any) => console.log(resposta)));
   }
 }
